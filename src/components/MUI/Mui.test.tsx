f31af2110a9548da20170b1201b15a63768be78b
Selvam <jselvamanu123@gmail.com>
2025-01-26 12:16:11 +0530

    intalling the lint-staged

--- src/components/MUI/Mui.test.tsx
+++ src/components/MUI/Mui.test.tsx
@@ -3,7 +3,8 @@ import Mui from './Mui';
 import React from 'react';
 import { AppThemeProvider } from '../../providers/ThemeProvider';
 import { render as customRender } from '../../utils/testUtil';
-
+console.log(data);
+const data = '1';
 describe('Testing MUI Component', () => {
   it('without theme provider', () => {
     render(<Mui />);
